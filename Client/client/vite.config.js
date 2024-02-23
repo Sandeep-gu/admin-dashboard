f4a803c31fd742f4a2bc91cbeee4d82183bf4f7d
sandeep gupta <sandeepkg8756@gmail.com>
2024-02-23 16:31:06 +0530

    changes

--- Client/client/vite.config.js
+++ Client/client/vite.config.js
@@ -4,10 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
-export default {
   build: {
     outDir: "build", // Adjust this to your desired directory
   },
-};
+});
